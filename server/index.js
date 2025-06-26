cc2173fc313e04cf5d202247861375ff27ff8f64
DhruvDabhi001 <dabhidhruv73@gmail.com>
2025-06-26 13:12:04 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@
   // Connect to MongoDB
   connectDB();
 
-  app.use(cors());
+  app.use(cors({
+    origin:
+    "https://ojas-frontend.onrender.com",
+    credentials: true
+  }));
   app.use(express.json());
 
   // Routes
